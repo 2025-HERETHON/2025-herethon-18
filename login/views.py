4f062d1b3de1d64e97a5e43df55a8a5b3e578277
yujining3827 <j_yujin_swu22@naver.com>
2025-07-08 20:29:24 +0900

    fix : html에 id, name 속성 추가

--- login/views.py
+++ login/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+# 로그인 페이지 : log_001
 def login(request):
     return render(request, 'login/log_001.html')
 
+# 회원가입 1 페이지 : log_002
 def setting_1(request):
     return render(request, 'login/log_002.html')
 
+# 회원가입 2 페이지 : log_003
 def setting_2(request):
     return render(request, 'login/log_003.html')
