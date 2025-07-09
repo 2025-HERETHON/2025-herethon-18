8218c0d5e23f67032c4a94702b2ab6cf8ac55ac9
yujining3827 <j_yujin_swu22@naver.com>
2025-07-10 01:42:52 +0900

    feat : 연령대별 홈 html 추가

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='main_002'),
+    path('infant/', views.home_infant, name='home_infant'),
+    path('middle/', views.home_middle, name='home_middle'),
+    path('teen/', views.home_teen, name='home_teen'),
+    path('youth/', views.home_youth, name='home_youth'),
+    path('elder/', views.home_elder, name='home_elder'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,18 @@ from login.models import User  # 홈 헤더에 연령대 표시
 
 def home(request):
     return render(request, 'home/main_002.html')
+
+def home_infant(request):
+    return render(request, 'home/home_infant.html')
+
+def home_teen(request):
+    return render(request, 'home/home_teen.html')
+
+def home_youth(request):
+    return render(request, 'home/home_youth.html')
+
+def home_middle(request):
+    return render(request, 'home/home_middle.html')
+
+def home_elder(request):
+    return render(request, 'home/main_elder.html')
