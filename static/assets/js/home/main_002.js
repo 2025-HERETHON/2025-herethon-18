30a19b83f816c2932763ff4303761751e0dfb2a5
yujining3827 <j_yujin_swu22@naver.com>
2025-07-10 01:23:39 +0900

    fix : main_002.js에서 페이지 변화 수정

--- static/assets/js/home/main_002.js
+++ static/assets/js/home/main_002.js
@@ -1,21 +1,21 @@
 // 헤더 상단 제목 클릭 시, main_002 화면으로 이동
 document.querySelector('.logo')?.addEventListener('click', function () {
-    window.location.href = 'main_002.html'
+    window.location.href = '/templates/templates/hoome/main_002.html'
 });
 
 // 상단 헤더 후기 버튼 클릭 시, rev_001로 이동
 document.querySelector('.review-btn')?.addEventListener('click', function () {
-    window.location.href='rev_001.html'
+    window.location.href='/templates/review/rev_001.html'
 });
 
 // 파란 박스 클릭 시, list_001 화면으로 이동
 document.querySelector('.popular-box-wrapper')?.addEventListener('click', function (){
-    window.location.href='list_001.html'
+    window.location.href='/templates/policyList/list_001.html'
 });
 
 // '생생한 후기 확인하기' 박스 하단 더보기 버튼 클릭 시, rev_001로 이동
 document.querySelector('.rev-more')?.addEventListener('click', function() {
-    window.location.href='rev_001.html'
+    window.location.href='/templates/review/rev_001.html'
 });
 
 // mypage 만들어졌을 경우 사용
