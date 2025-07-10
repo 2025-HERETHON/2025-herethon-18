f3ff546ee69530a31a1eeb7b12598503d569feff
2ewyeonwoo3 <yeonwooo10607@ewha.ac.kr>
2025-07-11 02:28:57 +0900

    fix: 메인헤더ui

--- static/assets/js/list.js
+++ static/assets/js/list.js
@@ -1,14 +1,18 @@
-// mypage 만들어졌을 경우 사용
-// 상단 헤더 후기 버튼 클릭 시, rev_001로 이동
-document.querySelector(".review-btn")?.addEventListener("click", function () {
-  window.location.href = "/review/list/";
-});
-// 헤더 상단 닉네임 클릭 시, mypage(myp_001)로 이동
-document.querySelector(".mypage-btn")?.addEventListener("click", function () {
-  window.location.href = "/mypage/";
-});
-
 document.addEventListener("DOMContentLoaded", function () {
+  // 헤더 상단 제목 클릭 시, main_002 화면으로 이동
+  document.querySelector(".logo")?.addEventListener("click", function () {
+    window.location.href = "/home/";
+  });
+
+  // 상단 헤더 후기 버튼 클릭 시, rev_001로 이동
+  document.querySelector(".review-btn")?.addEventListener("click", function () {
+    window.location.href = "/review/list/";
+  });
+  // 헤더 상단 닉네임 클릭 시, mypage(myp_001)로 이동
+  document.querySelector(".mypage-btn")?.addEventListener("click", function () {
+    window.location.href = "/mypage/";
+  });
+
   const currentPage = window.location.pathname.split("/").pop();
 
   document.querySelectorAll(".category_btn").forEach((link) => {
