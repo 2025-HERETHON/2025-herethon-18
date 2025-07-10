08a73abed3ce3f52dfe1db7f77966b15a93a77d9
2ewyeonwoo3 <yeonwooo10607@ewha.ac.kr>
2025-07-10 15:17:04 +0900

    feat: 완전 병합 완료

--- home/urls.py
+++ home/urls.py
@@ -8,9 +8,5 @@ urlpatterns = [
     path('teen/', views.home_teen, name='home_teen'),
     path('youth/', views.home_youth, name='home_youth'),
     path('elder/', views.home_elder, name='home_elder'),
+]
 
-<<<<<<< HEAD
-]
-=======
-]
->>>>>>> BE_feat/list
--- login/urls.py
+++ login/urls.py
@@ -5,8 +5,4 @@ urlpatterns = [
     path('', views.login, name='log_001'),
     path('setting_1/', views.setting_1, name='log_002'),
     path('setting_2/', views.setting_2, name='log_003'),
-<<<<<<< HEAD
 ]
-=======
-]
->>>>>>> BE_feat/list
