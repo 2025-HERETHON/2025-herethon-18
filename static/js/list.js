f300d3600a8635e917201c7ebc8f899db873e5db
jieun0102 <142320263+jieun0102@users.noreply.github.com>
2025-07-11 23:40:13 +0900

    fix: 헤더 닉네임 코드 수정

--- static/js/list.js
+++ static/js/list.js
@@ -29,27 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
-// 헤더 유저 닉네임으로 바뀌기 (더미데이터 사용)
-const dummyLoginResponse = {
-    accessToken: "eyJhbGciOiJIUzI1NiIs...",
-    refreshToken: "eyJhbGciOiJIUzI1NiIs...",
-    user: {
-        userId: 12,
-        nickname: "강아지가짱",
-        ageRange: "대학생·청년"
-    }
-};
-// localStorage에 저장 (세션 유지용)
-localStorage.setItem("user", JSON.stringify(dummyLoginResponse.user));
 
-document.addEventListener("DOMContentLoaded", () => {
-    const userData = JSON.parse(localStorage.getItem("user")); // 저장된 사용자 정보 불러오기
-    const nicknameSpan = document.getElementById("user-nickname");
-
-    if (userData && nicknameSpan) {
-        nicknameSpan.textContent = userData.nickname;
-    }
-});
 
 // 좋아요 수를 누르면 높은 순서로 정렬
 document.addEventListener("DOMContentLoaded", function () {
